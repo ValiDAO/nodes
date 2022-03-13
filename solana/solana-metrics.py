36dc97c602087df7b4af13fc8f28a9402fdd0e59
KOSTYAN <UNORDERED_SET@TUTANOTA.COM>
2022-03-13 03:00:59 +0000

    FIX

--- solana/solana-metrics.py
+++ solana/solana-metrics.py
@@ -29,14 +29,10 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write(str(self.get_balance()).encode("utf-8"))
         self.wfile.write(b"\n")
 
-        skip_rate, delinquent, leader_slots = self.get_skip_rate()
+        skip_rate, leader_slots = self.get_skip_rate()
         self.wfile.write(b"# TYPE solana_skiprate gauge\n")
         self.wfile.write(b"solana_skiprate ")
         self.wfile.write(str(skip_rate).encode("utf-8"))
-        self.wfile.write(b"\n")
-        self.wfile.write(b"# TYPE solana_delinquent gauge\n")
-        self.wfile.write(b"solana_delinquent ")
-        self.wfile.write(str(delinquent).encode("utf-8"))
         self.wfile.write(b"\n")
         self.wfile.write(b"# TYPE solana_leader_slots gauge\n")
         self.wfile.write(b"solana_leader_slots ")
@@ -56,17 +52,17 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
         my_leader_info = [li for li in block_data["leaders"] if li["identityPubkey"] == self.pubkey]
         if not my_leader_info:
-           return 0.0, 0.0, 0.0  # 100% skip as this requires investigation
+           return 0.0, 0.0  # 100% skip as this requires investigation
         my_leader_info = my_leader_info[0]
         skip_rate = my_leader_info["skippedSlots"] / my_leader_info["leaderSlots"]
-        delinquent = 1.0 if my_leader_info["delinquent"] else 0.0
         leader_slots = my_leader_info["leaderSlots"]
-        return skip_rate, delinquent, leader_slots
+        return skip_rate, leader_slots
 
     def get_credits(self):
         block_data = json.loads(subprocess.check_output(
             [BINARY_PATH, "block-production", "-ul", "--output=json"]
         ))
+        #delinquent = 1.0 if my_leader_info["delinquent"] else 0.0
 
   
 server = http.server.HTTPServer(('', 9114), MyHandler)
