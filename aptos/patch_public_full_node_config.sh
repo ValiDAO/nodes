1baa9d1afe619211bc7fa6c01179328e8fb59bfc
Kostyan <78592281+unordered-set@users.noreply.github.com>
2022-03-30 01:11:12 +0100

    Update patch_public_full_node_config.sh

--- aptos/patch_public_full_node_config.sh
+++ aptos/patch_public_full_node_config.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 KEY=$(cat "$SCRIPT_DIR/private-key")
-PEER_ID=$(cat "$SCRIPT_DIR/peer-info.yaml" | head -n2 | tail -n 1)
+PEER_ID=$(cat "$SCRIPT_DIR/peer-info.yaml" | head -n2 | tail -n 1 | cut -d: -f1)
 
 echo -e "/discovery_method/\na\n      identity:\n        type: \"from_config\"\n        key: \"$KEY\"\n        peer_id: \"$PEER_ID\"\n.\nwq" | ed "$SCRIPT_DIR/public_full_node.yaml"
