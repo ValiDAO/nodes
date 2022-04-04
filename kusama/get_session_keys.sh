1285349ca2cd67cc01ac02fbc707967011db2c72
Kostyan <78592281+unordered-set@users.noreply.github.com>
2022-04-05 00:47:19 +0100

    Update get_session_keys.sh

--- kusama/get_session_keys.sh
+++ kusama/get_session_keys.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-curl -H "Content-Type: application/json" -d'{"id":1, "jsonrpc":"2.0", "method": "author_rotateKeys", "params":[]}' htt
-p://localhost:9933
+curl -H "Content-Type: application/json" -d'{"id":1, "jsonrpc":"2.0", "method": "author_rotateKeys", "params":[]}' http://localhost:9933
