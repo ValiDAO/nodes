0b8a695646c91343add231556231c01ac1b45aac
KOSTYAN <UNORDERED_SET@TUTANOTA.COM>
2022-04-06 01:02:06 +0000

    Aptos: restart on network update

---
+++ aptos/restart_on_network_restart.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -x
+set -o pipefail
+
+VERSION_FILE="$HOME/aptos-core/version"
+
+PREV_VERSION="0"
+if [ -f $VERSION_FILE ]
+then
+   PREV_VERSION=$(cat $VERSION_FILE)
+fi
+
+LATEST_VERSION=$(curl 'https://aptos-devnet-stats.s3.us-west-2.amazonaws.com/current_version.json' --compressed -s | jq '.[0].value[1]' | sed -e 's/"//g')
+
+if [ "$PREV_VERSION" -gt "$LATEST_VERSION" ]
+then
+        echo "Restarting"
+        cd ~/aptos-core && git fetch origin devnet && git reset --hard origin/devnet
+        source ~/.cargo/env
+        cargo build -p aptos-node --release
+        systemctl stop aptos
+        rm -rf ~/aptos-data
+        systemctl start aptos
+else
+        echo "Doing nothing"
+fi
+
+echo $LATEST_VERSION > $VERSION_FILE
