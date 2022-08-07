b1a45cabe446533550050ad07d6d1a3da5e8445d
KOSTYAN <UNORDERED_SET@TUTANOTA.COM>
2022-08-07 23:37:13 +0000

    [SOLANA] check snapshot on start

--- solana/validator.sh
+++ solana/validator.sh
@@ -2,15 +2,53 @@
 
 source `dirname "${BASH_SOURCE[0]}"`/solana.env
 
-export RUST_LOG=info,solana_metrics=error
+SHOULD_START_CLEAN=1
+SLOT_DELAY_ALLOWANCE=2000
 
-echo "Downloading a snapshot"
-sudo docker run --rm \
-    -v {{ ansible_facts['env']['HOME'] }}/validator-ledger:/solana/snapshot \
-    --user $(id -u):$(id -g) \
-    kostya-downloader:latest \
-    --snapshot_path /solana/snapshot \
-    -r "$API_URL"
+SNAPSHOTS_CNT=$(find "{{ ansible_facts['env']['HOME'] }}/validator-ledger" -maxdepth 1 -name 'snapshot*tar.zst' | wc -l)
+if [ "$SNAPSHOTS_CNT" = 0 ]
+then
+	echo "SNAPSHOTS_CNT=$SNAPSHOTS_CNT is 0, doing reset"
+else
+	echo "SNAPSHOTS_CNT=$SNAPSHOTS_CNT is not 0, doing next check..."
+
+	BEST_KNOWN_INCREMENTAL_SLOT=$(cd {{ ansible_facts['env']['HOME'] }}/validator-ledger/ && find . -maxdepth 1 -name 'incremental-snapshot*tar.zst' | sort | tail -n1 | cut -d'-' -f4)
+	if [ -z "$BEST_KNOWN_INCREMENTAL_SLOT" ]
+	then
+		echo "BEST_KNOWN_INCREMENTAL_SLOT=$BEST_KNOWN_INCREMENTAL_SLOT is empty, doing reset"
+	else
+		echo "BEST_KNOWN_INCREMENTAL_SLOT=$BEST_KNOWN_INCREMENTAL_SLOT is not empty, doing next check..."
+		SLOT=$(solana slot)
+		echo "Current slot is $SLOT"
+		SLOT_DELAY=$((SLOT-BEST_KNOWN_INCREMENTAL_SLOT))
+		echo "Slot delay is $SLOT_DELAY"
+
+		if [ "$SLOT_DELAY" -gt "$SLOT_DELAY_ALLOWANCE" ]
+		then
+			echo "Slot delay $SLOT_DELAY is higher than $SLOT_DELAY_ALLOWANCE, doing reset"
+		else
+			echo "Slot delay $SLOT_DELAY is lower than $SLOT_DELAY_ALLOWANCE, just starting..."
+			SHOULD_START_CLEAN=0
+		fi
+	fi
+fi
+
+
+if [ "$SHOULD_START_CLEAN" = 1 ]
+then
+	echo "Cleaning..."
+	rm -rf "{{ ansible_facts['env']['HOME'] }}/validator-ledger"
+	mkdir -p "{{ ansible_facts['env']['HOME'] }}/validator-ledger"
+
+	echo "Downloading a snapshot"
+	sudo docker run --rm \
+	    -v {{ ansible_facts['env']['HOME'] }}/validator-ledger:/solana/snapshot \
+	    --user $(id -u):$(id -g) \
+	    kostya-downloader:latest \
+	    --snapshot_path /solana/snapshot \
+	    -r "$API_URL"
+fi
+
 
 
 nice -n -20 solana-validator \
